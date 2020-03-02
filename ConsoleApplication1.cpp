fe8a2c0450b13125c47e51b0d3446f5e15ceb056
Sauravmakam30 <61684673+Sauravmakam30@users.noreply.github.com>
2020-03-02 16:29:20 +0530

    Add files via upload
    Question 12

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,71 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main(int argc,char *argv[])
+{
+    int value;
+        value = atoi(argv[1]);
+	if (value % 2 == 0)
+        {
+            cout << "<---Entered number is invalid--->\n";
+        }
+        else
+        {
+            for (int i = 1; i <= value; i++)
+            {
+                for (int j = 1; j <= value; j++)
+                {
+                    if (i == 1 || i == value || j == 1 || j == value || i == ((value + 1) / 2) || j == ((value + 1) / 2))
+                    {
+                        cout << "*";
+                    }
+                    else if (i == j && i < ((value + 1) / 2))
+                    {
+                        cout << "*";
+                    }
+                    else if (abs(i - j) == ((value - 1) / 2) && i < ((value + 1) / 2))
+                    {
+                        cout << "*";
+                    }
+                    else if ((i + j) == (value + 1) && i > ((value + 1) / 2))
+                    {
+                        cout << "*";
+                    }
+                    else if ((i + j) == (value + (value + 1) / 2))
+                    {
+                        cout << "*";
+                    }
+                    else
+                    {
+                        cout << " ";
+                    }
+                }
+                cout << "\n";
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Readme.txt
@@ -0,0 +1,23 @@
+TEAM DETAILS:
+• Saurav Makam 1KS18CS089
+• SURAJ J 1KS18CS034
+TEAM CONTRIBUTION:
+• Saurav Logic and coding
+• Suraj Coding and debugging
+• INSTRUCTIONS:
+• After execution of program there is an line to enter any odd number.
+• Enter any of the odd number.
+DETAILS ON EXAMPLE INVOCATION AND OUTPUT:
+• Input:
+15
+• Output:
+ATTACHED 
+CHALLENGES FACED:
+• Cracking the code to represent the diagonals.
+• Providing efficient solution.
+• Decoding when errors found.
+LEARNT FROM THE ASSIGNMENT:
+• Effective coding.
+• Logical thinking and to prove it mathematically.
+OPERATION:
+• The number of basic operations performed is given with output. 
